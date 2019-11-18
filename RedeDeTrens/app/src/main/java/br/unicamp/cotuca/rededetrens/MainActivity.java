2bd441cde26aeee9c47fa5a413c98f84b43d3857
kenjinBomDeBola <43621085+kenjinBomDeBola@users.noreply.github.com>
2019-11-18 11:53:06 -0300

    sd

--- RedeDeTrens/app/src/main/java/br/unicamp/cotuca/rededetrens/MainActivity.java
+++ RedeDeTrens/app/src/main/java/br/unicamp/cotuca/rededetrens/MainActivity.java
@@ -3,12 +3,44 @@ package br.unicamp.cotuca.rededetrens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.io.File;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        try {
+            TextView tvResultado = findViewById(R.id.txtViewResultados);
+            setContentView(R.layout.activity_main);
+            Scanner sc = new Scanner(new File("C:\\Temp\\GrafoTremEspanhaPortugal.txt").getAbsolutePath());
+            String[] array = new String[500];
+            for (int i = 0; sc.hasNextLine(); i++) {
+                array[i] = sc.nextLine();
+            /*
+            Spinner spnD = (Spinner) findViewById(R.id.spnDe);
+            Spinner spnP = (Spinner) findViewById(R.id.spnPara);
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_spinner_item,sc.nextLine())
+
+             */
+
+            }
+            TextView teste = (TextView) findViewById(R.id.txtViewResultados);
+            teste.setText(array[1]);
+            sc.close();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
     }
 }
