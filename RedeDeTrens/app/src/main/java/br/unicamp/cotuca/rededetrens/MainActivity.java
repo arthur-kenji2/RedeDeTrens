eb7a0bc2c7cfc9166b2530e89b4a543ba88f61bb
kenjinBomDeBola <43621085+kenjinBomDeBola@users.noreply.github.com>
2019-11-22 11:10:35 -0300

    a

--- RedeDeTrens/app/src/main/java/br/unicamp/cotuca/rededetrens/MainActivity.java
+++ RedeDeTrens/app/src/main/java/br/unicamp/cotuca/rededetrens/MainActivity.java
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.view.*;
+import android.widget.*;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.io.*;
+import java.text.NumberFormat;
 import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,23 +25,52 @@ public class MainActivity extends AppCompatActivity {
             TextView tvResultado = findViewById(R.id.txtViewResultados);
             setContentView(R.layout.activity_main);
             AssetManager ass = getAssets();
-            Scanner sc = new Scanner(ass.open("GrafoTrem.txt"));
+            Scanner sc = new Scanner(ass.open("GrafoTrem"));
             String s = "";
-            List<String> lista = new ArrayList<String>();
-            for (int i = 0; sc.hasNext(); i++) {
+            ArrayList<String> lista = new ArrayList<String>();
+            for (int i = 0; sc.hasNext(); i++)
+            {
                 s = sc.next();
-                lista.add(s);
-            /*
+                if(!isNumber(s))
+                {
+                    if(!jaTem(s, lista))
+                        lista.add(s);
+                }
+            }
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, lista);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
             Spinner spnD = (Spinner) findViewById(R.id.spnDe);
             Spinner spnP = (Spinner) findViewById(R.id.spnPara);
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                    android.R.layout.simple_spinner_item,sc.nextLine())
+            spnD.setAdapter(adapter);
+            spnP.setAdapter(adapter);
+            spnD.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+            {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+                {
+                    String st = parent.getItemAtPosition(position).toString();
+                    Toast.makeText(parent.getContext(), "Selected: " + st,Toast.LENGTH_LONG).show();
+                }
+                @Override
+                public void onNothingSelected(AdapterView <?> parent)
+                {
+                }
+            });
+            spnP.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+            {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+                {
+                    String st = parent.getItemAtPosition(position).toString();
+                    Toast.makeText(parent.getContext(), "Selected: " + st,Toast.LENGTH_LONG).show();
+                }
+                @Override
+                public void onNothingSelected(AdapterView <?> parent)
+                {
+                }
+            });
 
-             */
-
-            }
-            TextView teste = (TextView) findViewById(R.id.txtViewResultados);
-                teste.setText(lista.get(0).toString());
             sc.close();
         }
         catch (Exception e)
@@ -46,5 +78,27 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
+    }
+
+    public boolean isNumber(String n)
+    {
+        try{
+            Integer.parseInt(n);
+            return true;
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+
+    public boolean jaTem(String n, List lista)
+    {
+        for(int i = 0; i < lista.size();i++)
+            if(lista.get(i).equals(n))
+                return true;
+            return false;
     }
 }
